546ec360a7339d260113b9e3c9afb4c30f2aa45e
v3nd3774 <josue.caraballo@gmail.com>
2024-02-10 20:10:54 -0600

    fix: 🐛 typing issue

--- selenium/visualization_check.py
+++ selenium/visualization_check.py
@@ -2,7 +2,7 @@
 Browses to frontend and waits for bipartite graph visualization to load.
 """
 from time import sleep, time
-from typing import List
+from typing import List, Union
 
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry import metrics
@@ -56,7 +56,7 @@ driver.get("https://idir.uta.edu/bipartiteGraphUi/")
 driver.implicitly_wait(1)
 RETRIES: int = 15
 SLEEP_TIME: int = 10
-bipartite_graph_svg: WebElement | None = None
+bipartite_graph_svg: Union[WebElement, None] = None
 while RETRIES >= 0:
     try:
         bipartite_graph_svg = driver.find_element(
